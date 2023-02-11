70a94bdef8c157395a2f7dedf99ec38dfbcf4120
Abdul Ragib <abdulragib36@gmail.com>
2023-02-11 15:32:06 +0530

    Solution 4.0

--- src/App.js
+++ src/App.js
@@ -1,81 +1,82 @@
-import React, { useState } from 'react';
-import './App.css';
-import Inputgroup from "./component/Inputgroup/Inputgroup"
-import Operations from "./component/Operations/Operations"
+import React, { useState } from "react";
+import "./App.css";
+import Inputgroup from "./component/Inputgroup/Inputgroup";
+import Operations from "./component/Operations/Operations";
 
 function App() {
+  const [input1, setinput1] = useState("");
+  const [input2, setinput2] = useState("");
+  const [Validate, setValidate] = useState("");
+  const [result, setResult] = useState(0);
+  const [showResult, setShowResult] = useState(false);
 
-  const [input1,setinput1]=useState('');
-  const [input2,setinput2]=useState('');
-  const [Validate,setValidate]=useState('');
-  const [result,setResult]=useState(0);
-  const [showResult,setShowResult]=useState(false);
-  
+  const handleinputOne = (e) => {
+    setinput1(e.target.value);
+  };
 
-  const handleinputOne=(e)=>{
-    setinput1(e.target.value)
-  }
+  const handleinputTwo = (e) => {
+    setinput2(e.target.value);
+  };
 
-  const handleinputTwo=(e)=>{
-    setinput2(e.target.value)
-  }
-
-  const validate=()=>{
-    if(input1===''){
-       setValidate('Error : Num1 cannot be empty');
-       setShowResult(false)
-       return false;
-    }
-    else if(input2==='')
-    {
-      setValidate('Error : Num2 cannot be empty');
-      setShowResult(false)
+  const validate = () => {
+    if (input1 === "") {
+      setValidate("Error : Num1 cannot be empty");
+      setShowResult(false);
       return false;
-    }
-    else{
-      setValidate('Your Result is shown above');
-      setShowResult(true)
+    } else if (input2 === "") {
+      setValidate("Error : Num2 cannot be empty");
+      setShowResult(false);
+      return false;
+    } else {
+      setValidate("Your Result is shown above");
+      setShowResult(true);
       return true;
     }
-  }
+  };
 
-  const addition=()=>{
+  const addition = () => {
     if (!validate()) return;
-    let result=parseInt(input1)+parseInt(input2);
+    let result = parseInt(input1) + parseInt(input2);
     setResult(result);
     setShowResult(true);
-  }
+  };
 
-  const substraction=()=>{
+  const substraction = () => {
     if (!validate()) return;
-    let result=parseInt(input1)-parseInt(input2);
+    let result = parseInt(input1) - parseInt(input2);
     setResult(result);
     setShowResult(true);
-  }
+  };
 
-  const multiply=()=>{
+  const multiply = () => {
     if (!validate()) return;
-    let result=parseInt(input1)*parseInt(input2);
+    let result = parseInt(input1) * parseInt(input2);
     setResult(result);
     setShowResult(true);
-  }
+  };
 
-  const division=()=>{
+  const division = () => {
     if (!validate()) return;
-    let result=parseInt(input1)/parseInt(input2);
+    let result = parseInt(input1) / parseInt(input2);
     setResult(result);
     setShowResult(true);
-  }
-
+  };
 
   return (
     <div className="App">
       {<h1>React Calculator</h1>}
-     
-      <Inputgroup onChangeOne={handleinputOne} onChangeTwo={handleinputTwo}/>
-      <Operations addition={addition} substraction={substraction}  multiply={multiply} division={division}/>
+
+      <Inputgroup onChangeOne={handleinputOne} onChangeTwo={handleinputTwo} />
+      <Operations
+        addition={addition}
+        substraction={substraction}
+        multiply={multiply}
+        division={division}
+      />
       {showResult && <div>Result = {result}</div>}
-      <div style={{ color: Validate.includes('Error') ? 'red' : 'green' }}>{Validate}</div>
+      <div style={{ color: Validate.includes("Error") ? "red" : "green" }}>
+        {Validate}
+      </div>
     </div>
   );
 }
--- src/component/Operations/Operations.js
+++ src/component/Operations/Operations.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import './Operations.css';
+import React from "react";
+import "./Operations.css";
 
-const Operations = ({addition,substraction,multiply,division}) =>{
-    return(
-        <div>
-            <button onClick={()=>addition()}>+</button>
-            <button onClick={()=>substraction()}>-</button>
-            <button onClick={()=>multiply()}>*</button>
-            <button onClick={()=>division()}>/</button>
-        </div>
-    )
-}
+const Operations = ({ addition, substraction, multiply, division }) => {
+  return (
+    <div>
+      <button onClick={() => addition()}>+</button>
+      <button onClick={() => substraction()}>-</button>
+      <button onClick={() => multiply()}>*</button>
+      <button onClick={() => division()}>/</button>
+    </div>
+  );
+};
 
 export default Operations;
